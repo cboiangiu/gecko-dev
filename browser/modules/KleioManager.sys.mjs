2e7531bc954630566c80d1190abc09554ec7ad81
cboiangiu <catalin.boiangiu@lansoft.ro>
2023-10-09 12:35:12 +0300

    handle amazon redirect

--- browser/modules/KleioManager.sys.mjs
+++ browser/modules/KleioManager.sys.mjs
@@ -4,6 +4,7 @@ class _KleioManager {
     windowsService = Components.classes['@mozilla.org/appshell/window-mediator;1'].getService(Components.interfaces.nsIWindowMediator);
     lastRedirectTimestamp = Date.now() - 2000;
     lastSyncTimestamp = Date.now() - 1800000;
+    amazonRegex = /https:\/\/www\.amazon\.[A-Za-z.]+\/(?:[A-Za-z0-9-]+\/)?dp\/([A-Za-z0-9]+)\/?/;
 
     getCurrentURI = () => {
         // window object representing the most recent (active) instance of Firefox
@@ -19,7 +20,7 @@ class _KleioManager {
         return referredFromURI;
     }
 
-    checkIfRedirectIsNeeded = (host) => {
+    checkIfRedirectIsNeeded = (host, url) => {
         let localKey = null;
         for (const [key] of this.redirects) {
             if (host.includes(key)) {
@@ -32,12 +33,26 @@ class _KleioManager {
         }
 
         const referredFromURI = this.getCurrentURI();
-        if (referredFromURI?.host.includes(localKey)) {
-            return false;
-        }
-        for (const provider of this.providers) {
-            if (referredFromURI?.host.includes(provider)) {
+        // handles special amazon use case
+        if (localKey.includes("amazon.")) {
+            const hostProductIdMatch = url.match(this.amazonRegex);
+            if (hostProductIdMatch == null) {
                 return false;
+            }
+            const hostProductId = hostProductIdMatch[1];
+            const referredFromURIProductIdMatch = referredFromURI?.spec.match(this.amazonRegex);
+            const referredFromURIProductId = referredFromURIProductIdMatch == null ? null : referredFromURIProductIdMatch[1];
+            if (hostProductId == referredFromURIProductId) {
+                return false;
+            }
+        } else {
+            if (referredFromURI?.host.includes(localKey)) {
+                return false;
+            }
+            for (const provider of this.providers) {
+                if (referredFromURI?.host.includes(provider)) {
+                    return false;
+                }
             }
         }
         if (this.lastRedirectTimestamp + 2000 > Date.now()) {
@@ -46,11 +61,17 @@ class _KleioManager {
         return true;
     }
 
-    makeRedirect = (redirectTo) => {
+    makeRedirect = (redirectTo, host) => {
         let encodedRedirectTo = encodeURIComponent(redirectTo);
         let redirect = null;
         for (const [key, value] of this.redirects) {
             if (redirectTo.includes(key)) {
+                // handles special amazon use case
+                if (host.includes("amazon.")) {
+                    let redirectToProductId = redirectTo.match(this.amazonRegex)[1];
+                    redirect = value.replace("ENCODED_REDIRECT_TO", redirectToProductId);
+                    break;
+                }
                 redirect = value.replace("ENCODED_REDIRECT_TO", encodedRedirectTo);
                 break;
             }
@@ -92,10 +113,10 @@ class _KleioManager {
                 const httpChannel = subject.QueryInterface(Ci.nsIHttpChannel);
                 const host = httpChannel.URI.host;
                 const url = httpChannel.URI.spec;
-                if (this.checkIfRedirectIsNeeded(host)) {
+                if (this.checkIfRedirectIsNeeded(host, url)) {
                     // console.log("Initial url: " + this.getCurrentURI()?.spec);
                     // console.log("Redirecting for: " + url);
-                    const redirect = this.makeRedirect(url);
+                    const redirect = this.makeRedirect(url, host);
                     if (redirect !== null) {
                         this.lastRedirectTimestamp = Date.now();
                         const newURI = Services.io.newURI(redirect, null, null);
